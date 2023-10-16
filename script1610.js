36fee8bf3788529b2fe5a9315f16eddc430b607d
jstudnic1 <studnicka.jakub04@gmail.com>
2023-10-16 14:44:21 +0200

    first push

---
+++ script1610.js
@@ -0,0 +1,9 @@
+let number = 15;
+
+if (number % 3 === 0 && number % 5 === 0){
+    console.log("FizzBuzz");
+} else if (number % 3 === 0){
+    console.log("Fizz");
+}else if (number % 5 === 0){
+    console.log("Buzz");
+}
